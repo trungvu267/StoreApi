6964f5bbd488cce103b8ecb94be90b92353eb032
TrungVu <vutrung26072001@gmail.com>
2022-12-03 18:20:45 +0700

    updte product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,17 +2,41 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 namespace StoreApi.Models;
 
+public class Rating
+{
+    [BsonElement("rate")]
+    public double Rate { get; set; }
+    [BsonElement("count")]
+    public int Count { get; set; }
+    public Rating(double rate, int count)
+    {
+        Rate = rate;
+        Count = count;
+    }
+}
 public class Product
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
-    [BsonElement("name")]
-    public string productName { get; set; } = null!;
+    [BsonElement("price")]
+    public double Price { get; set; } 
+    [BsonElement("title")]
 
-    public float price { get; set; }
+    public string Title { get; set; } = null!;
+    [BsonElement("description")]
 
-    public string category { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    [BsonElement("category")]
+
+    public string Category { get; set; } = null!;
+    [BsonElement("image")]
+
+    public string Image { get; set; } = null!;
+    [BsonElement("rating")]
+
+
+    public Rating? Rating {get;set;} 
 
 }
